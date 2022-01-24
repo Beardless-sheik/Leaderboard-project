17e105b686a471b78bddc76f2b1f1a2b89395fcd
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-24 21:58:29 +0200

    Create draft of first addscore async method to post to the Leadership API

--- src/modules/crud.js
+++ src/modules/crud.js
@@ -1,9 +1,22 @@
-const addScore = (name, score, scoresArray) => {
+const addScore = async (name, score, apiEndPoint, idLink, htmlElementToPostSuccesfulMessage) => {
   const newScoreObject = {
-    name,
+    user: name,
     score,
   };
-  return [...scoresArray, newScoreObject];
+  let responseFromServer = ''
+  await fetch(`${apiEndPoint}${idLink}/scores/`, {
+    method: 'POST',
+    body: JSON.stringify(newScoreObject),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  }).then((response) => {
+    const data = response.json();
+    responseFromServer = data.result;
+  }).catch((error) => {
+    responseFromServer = error.message;
+  });
+  htmlElementToPostSuccesfulMessage.innerHTML = responseFromServer;
 };
 
 export default addScore;
