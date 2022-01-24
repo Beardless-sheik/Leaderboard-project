507f3386354f04ea20bf7bf52a7fa2be446fbcb7
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-24 23:02:36 +0200

    Readjust render methods to return messages based on server interactions

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,12 +1,16 @@
 const renderScores = (scoresArray, htmlParentToRenderTo) => {
   htmlParentToRenderTo.innerHTML = '';
-  scoresArray.forEach((element, index) => {
-    if (index % 2 === 0) {
-      htmlParentToRenderTo.innerHTML += `<li> ${element.name}: ${element.score} </li>`;
-    } else {
-      htmlParentToRenderTo.innerHTML += `<li class="colured-background"> ${element.name}: ${element.score} </li>`;
-    }
-  });
+  if (scoresArray.length === 0) {
+    htmlParentToRenderTo.innerHTML += '<li> No names or scores are present <li>';
+  } else {
+    scoresArray.forEach((element, index) => {
+      if (index % 2 === 0) {
+        htmlParentToRenderTo.innerHTML += `<li> ${element.name}: ${element.score} </li>`;
+      } else {
+        htmlParentToRenderTo.innerHTML += `<li class="colured-background"> ${element.name}: ${element.score} </li>`;
+      }
+    });
+  }
 };
 
 export default renderScores;
