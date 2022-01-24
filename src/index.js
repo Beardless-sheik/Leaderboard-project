c8b03cfdd73254cac6dee7a691e382a1488cb63e
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-24 23:01:50 +0200

    Add async/ await functions for local storage items

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './style.css';
 import { saveToLocalStorage, retrieveFromLocalStorage } from './modules/localStorage.js';
 import renderScores from './modules/render.js';
-import addScore from './modules/crud.js';
+import { addScore, getScores } from './modules/crud.js';
 
+const gameID = 'Bus6JlqV8NG7oMQ0k9es/';
+const apiEndPoint = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const serverMessageElement = document.querySelector('#space-for-server-message');
 const scoreForm = document.querySelector('.form-container');
 const nameInput = document.querySelector('#userName');
 const scoreInput = document.querySelector('#userScore');
@@ -10,16 +13,19 @@ const scoreUlContainer = document.querySelector('#results-container');
 
 let scoresArray = [];
 
-const submitFormFunction = (event) => {
+const submitFormFunction = async (event) => {
   event.preventDefault();
-  scoresArray = addScore(nameInput.value, scoreInput.value, scoresArray);
-  saveToLocalStorage(scoresArray);
+  await addScore(nameInput.value, scoreInput.value, apiEndPoint, gameID, serverMessageElement);
+  // saveToLocalStorage(scoresArray);
+  scoresArray = await getScores(apiEndPoint, gameID, serverMessageElement);
   renderScores(scoresArray, scoreUlContainer);
   scoreForm.reset();
 };
 
 scoreForm.addEventListener('submit', submitFormFunction);
-window.addEventListener('load', () => {
-  scoresArray = retrieveFromLocalStorage();
+window.addEventListener('load', async() => {
+  // scoresArray = retrieveFromLocalStorage();
+  scoresArray = await getScores(apiEndPoint, gameID, serverMessageElement);
+  console.log(scoresArray);
   renderScores(scoresArray, scoreUlContainer);
 });
