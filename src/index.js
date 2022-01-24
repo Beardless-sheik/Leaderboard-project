789b74427a850aff678113649a42657b6616be77
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-01-24 21:16:21 +0200

    Update index.js
    - Pick HTML items from the DOM
    - Add scaffolding functions
    - Import functions from other modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+import { saveToLocalStorage, retrieveFromLocalStorage } from './modules/localStorage.js';
+import renderScores from './modules/render.js';
+
+const scoreForm = document.querySelector('.form-container');
+const nameInput = document.querySelector('#userName');
+const scoreInput = document.querySelector('#userScore');
+const scoreUlContainer = document.querySelector('#results-container');
+
+const scoresArray = [];
+
+renderScores(scoresArray, scoreUlContainer);
+
+const submitFormFunction = (event) => {
+  // event.preventDefault();
+  // console.log(nameInput.value, scoreInput.value);
+  // scoreForm.reset();
+}
+
+scoreForm.addEventListener('submit', submitFormFunction);
+// window.addEventListener('load', () => {
+//   scoresArray
+// })
